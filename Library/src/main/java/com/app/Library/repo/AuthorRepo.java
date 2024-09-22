07e894b6c6b9040c604a982f409b9353428e224b
domkar764 <87006613+domkar764@users.noreply.github.com>
2024-09-23 02:44:54 +0530

    Delete Library directory

--- Library/src/main/java/com/app/Library/repo/AuthorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.Library.repo;
-
-import com.app.Library.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepo extends JpaRepository<Author,Integer > {
-}
--- Library/src/main/java/com/app/Library/service/AuthorService.java
+++
@@ -1,7 +0,0 @@
-package com.app.Library.service;
-
-import com.app.Library.model.Author;
-
-public interface AuthorService {
-    void save(Author author);
-}
